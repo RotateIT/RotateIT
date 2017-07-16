f8e77011211e2a55b779ddd1a400a5e13f79614d
ThePnx <lukasz.jeczmienny@gmail.com>
2017-07-16 22:51:34 +0200

    Add test for LabelWebhookController

--- src/main/java/rotateit/github/domain/User.java
+++ src/main/java/rotateit/github/domain/User.java
@@ -2,10 +2,12 @@ package rotateit.github.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
+import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
+
 public class User {
     private final String login;
 
-    @JsonCreator
+    @JsonCreator(mode = PROPERTIES)
     public User(String login) {
         this.login = login;
     }
