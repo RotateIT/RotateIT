b2a01e6b720b335daeaa34cf79d81c779fcb255b
ThePnx <lukasz.jeczmienny@gmail.com>
2017-08-03 20:57:31 +0200

    ReviewActionEvents accepts LabelPayload as source

--- src/main/java/rotateit/domain/github/label/event/factory/ReadyForReviewEventFactory.java
+++ src/main/java/rotateit/domain/github/label/event/factory/ReadyForReviewEventFactory.java
@@ -1,11 +1,12 @@
 package rotateit.domain.github.label.event.factory;
 
 import org.springframework.context.ApplicationEvent;
+import rotateit.domain.github.label.LabelPayload;
 import rotateit.domain.github.label.event.ReadyForReviewEvent;
 
 public class ReadyForReviewEventFactory implements ReviewActionEventFactory {
     @Override
-    public ApplicationEvent createReviewActionEvent(Object source) {
-        return new ReadyForReviewEvent(source);
+    public ApplicationEvent createReviewActionEvent(LabelPayload labelPayload) {
+        return new ReadyForReviewEvent(labelPayload);
     }
 }
--- src/main/java/rotateit/domain/github/label/event/factory/UnknownReviewActionEventFactory.java
+++ src/main/java/rotateit/domain/github/label/event/factory/UnknownReviewActionEventFactory.java
@@ -1,11 +1,12 @@
 package rotateit.domain.github.label.event.factory;
 
 import org.springframework.context.ApplicationEvent;
+import rotateit.domain.github.label.LabelPayload;
 import rotateit.domain.github.label.event.UnknownReviewActionEvent;
 
 public class UnknownReviewActionEventFactory implements ReviewActionEventFactory {
     @Override
-    public ApplicationEvent createReviewActionEvent(Object source) {
-        return new UnknownReviewActionEvent(source);
+    public ApplicationEvent createReviewActionEvent(LabelPayload labelPayload) {
+        return new UnknownReviewActionEvent(labelPayload);
     }
 }
